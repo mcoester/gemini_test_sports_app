fdb185aa94fa546392f453b3ef3aa88078b22185
Michael Cöster <michaelcoester@googlemail.com>
2025-08-13 15:24:48 +0200

    Another try

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://mcoester.github.io/gemini_test_sports_app/',
 })
